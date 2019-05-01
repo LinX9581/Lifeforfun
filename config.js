3f89da7698bcbada4c4dde5fa01bc7593acb53dc
linx9581 <linx9581@gmail.com>
2019-05-01 12:26:59 +0800

    remove unnecesary files

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'GITHUB_KEY': '3ef6d29f043fd07f2a68',
-    'GITHUB_SECRET': 'e84bc7a9c23d3e4b4e552fdbd1fd11fb21c10458'
-}
--- test.js
+++
@@ -1,7 +0,0 @@
-const a = 5;
-
-const b = 5;
-
-if (a == b) {
-    console.log("test");
-}
